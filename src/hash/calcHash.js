c9c5b55831d115c89d809b526be45174dcdf563c
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-29 06:58:00 -0700

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'node:crypto';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const readableStream = createReadStream(filePath);
+
+  for await (const chunk of readableStream) {
+    hash.update(chunk);
+  }
+
+  stdout.write(hash.digest('hex'));
 };
 
 await calculateHash();
+
+// calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API
