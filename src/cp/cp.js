605945573040ce75fa90039cc09b92c53dbe18e9
barodzichaliaskandr@gmail.com <borodichalexander97@gmail.com>
2024-01-29 06:58:20 -0700

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = join(__dirname, 'files', 'script.js');
+  fork(filePath, args, { stdio: [0, 1, 2, 'ipc'] });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--some-arg', 'value1', '--other', '1337', '--arg2', '42']);
