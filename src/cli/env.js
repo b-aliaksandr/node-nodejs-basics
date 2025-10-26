b2a059465e7a0ce54050dbf5fda2bf3d9e78027e
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-10-26 23:40:39 +0300

    feat: implement cli:env fn

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+import { env, stdout } from 'node:process';
+
 const parseEnv = () => {
-  // Write your code here
+  const prefix = 'RSS_';
+  const vars = Array
+    .from(Object.entries(env))
+    .filter(([key, value]) => {
+      if (key.startsWith(prefix)) return [key, value];
+    });
+  const outputStr = vars
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+  stdout.write(outputStr);
+  stdout.write('\n');
 };
 
 parseEnv();
