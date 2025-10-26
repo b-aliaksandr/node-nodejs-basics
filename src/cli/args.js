48396da39820e2cebbc653559e6d7600cc0ad8ce
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-10-26 23:46:24 +0300

    feat: implement cli:args fn

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,28 @@
+import { argv, stdout } from 'node:process';
+
 const parseArgs = () => {
-  // Write your code here
+  const props = [];
+  console.log(argv)
+
+  let isPropPrevArg = false;
+  argv.forEach((value) => {
+    if (value.startsWith('--')) {
+      props.push({ name: value });
+      isPropPrevArg = true;
+    } else {
+      if (isPropPrevArg) {
+        const prop = props.at(-1);
+        prop.value = value;
+      }
+      isPropPrevArg = false;
+    }
+  });
+  const outputStr = props
+    .map(({ name, value }) => `${name} is ${value}`)
+    .join(', ');
+
+  stdout.write(outputStr);
+  stdout.write('\n');
 };
 
 parseArgs();
