d8fca9bdc10894741c3756095daeb0aada0c0cbe
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-10-27 00:32:53 +0300

    feat: implement zip:decompress fn

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-  // Write your code here
+  const srcFilePath = join(__dirname, 'files', 'archive.gz');
+  const destFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+
+  const source = createReadStream(srcFilePath);
+  const destination = createWriteStream(destFilePath);
+
+  try {
+    await pipeline(source, createGunzip(), destination);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await decompress();
