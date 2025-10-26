9c9d2f99e1debbd9bce9f758bfbddf767e892c78
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-10-26 21:04:11 +0300

    feat: implement fs:delete fn

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { rm } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-  // Write your code here
+  const FOLDER_PATH = join(__dirname, 'files');
+  const fileNamePath = join(FOLDER_PATH, 'fileToRemove.txt');
+
+  try {
+    await rm(fileNamePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+    console.error(err);
+  }
 };
 
 await remove();
