bcbb188d85f1033edcb7cda889aac6857a377f45
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-10-27 00:25:12 +0300

    feat: implement streams:write fn

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const writableStream = createWriteStream(filePath);
+  stdin.pipe(writableStream);
 };
 
 await write();
